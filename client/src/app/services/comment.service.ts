bef4b0412e43f59781a08bb725357cf622e70da5
SoftHoarders <s.hoarders@gmail.com>
2021-12-12 08:10:53 +0200

    fixed last commit

--- client/src/app/services/comment.service.ts
+++ client/src/app/services/comment.service.ts
@@ -7,8 +7,9 @@ export class CommentService {
   constructor() {}
   getComments(){
       ///chestii complicate https nu stiu 
-      return [{userName:"pisat", title: "cacat", content: "lorem ipsum dolor", rating: 3},{userName:"pisat", title: "cacat", content: "lorem ipsum dolor", rating: 3},{userName:"pisat", title: "cacat", content: "lorem ipsum dolor", rating: 3}]
+      return [{userName:"Username", title: "Lorem Ipsum Dolor", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut purus quis nisl pharetra pulvinar. Etiam in erat nec tellus porttitor lacinia. Sed et justo in nisi dapibus hendrerit eu ac libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut commodo lectus. Proin ornare, nibh ac bibendum tempor, leo lacus pretium ex, eget finibus nunc nulla nec ipsum. Mauris tempus metus suscipit, fermentum libero eu, vulputate odio.", rating: 3},
+              {userName:"Username", title: "Lorem Ipsum Dolor", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut purus quis nisl pharetra pulvinar. Etiam in erat nec tellus porttitor lacinia. Sed et justo in nisi dapibus hendrerit eu ac libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut commodo lectus. Proin ornare, nibh ac bibendum tempor, leo lacus pretium ex, eget finibus nunc nulla nec ipsum. Mauris tempus metus suscipit, fermentum libero eu, vulputate odio.", rating: 1},
+              {userName:"Username", title: "Lorem Ipsum Dolor", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut purus quis nisl pharetra pulvinar. Etiam in erat nec tellus porttitor lacinia. Sed et justo in nisi dapibus hendrerit eu ac libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut commodo lectus. Proin ornare, nibh ac bibendum tempor, leo lacus pretium ex, eget finibus nunc nulla nec ipsum. Mauris tempus metus suscipit, fermentum libero eu, vulputate odio.", rating: 4}
+             ]
   }
-  
-  
 }
